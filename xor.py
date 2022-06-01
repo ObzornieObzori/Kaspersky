21e0ed873c9eb8d6218635c595840a1eea967813
ObzornieObzori <106546384+ObzornieObzori@users.noreply.github.com>
2022-06-01 20:27:50 +0300

    шифр Вермана

---
+++ xor.py
@@ -0,0 +1,10 @@
+def xor(t):
+    alph="qwertyuiopasdfghjklzxcvbnm_{}1234567890"
+    gamma="thekey"
+    ans=''
+    g=0
+    for i in t:
+        ans+=alph[int((alph.index(i)) + int(alph.index(gamma[g]) % 6)) % 39]
+    g+=1
+    return ans
+print(xor(input('Введите сообщение: ')))
